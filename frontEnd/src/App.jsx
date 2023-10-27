625e202468e0f5a026f186987b3dea6cc4301044
Sahil <leviack629@gmail.com>
2023-10-27 19:12:00 +0530

    frontend

---
+++ frontEnd/src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  const [fruits, setFruits] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/fruits")
+      .then((response) => {
+        setFruits(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  return (
+    <>
+      <h1>Hello FullStack!</h1>
+      <h4>a simple app to learn how to create backend server and then attach it to the frontend</h4>
+      <p>FRUITS: {fruits.length} </p>
+      {fruits.map((fruit, index) => {
+        <div key={fruit.id}>
+          <h3>{fruit.title}</h3>
+          <p>{fruit.content}</p>
+        </div>;
+      })}
+    </>
+  );
+}
+
+export default App;
